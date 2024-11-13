7f09fb7e82fb816bbe06e14517ccb76f9c052eed
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-11-12 19:15:34 -0700

    perfectSquares
    ok honestly I think we should go back and make this a knapsack algorithm instead or whatever, where we start from the bottom and count up bc like I think that would be more space efficient maybe.

---
+++ perfectSquares.py
@@ -0,0 +1,57 @@
+from tkinter.font import names
+import numpy
+from math import inf
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        target = n
+        total = 0
+        upper_bound = numpy.sqrt(target) # not sure if it will let me use that
+        upper_bound = upper_bound.astype(int)
+
+        i = upper_bound
+        j = upper_bound
+        total_dict = {}
+
+        for i in range(upper_bound, 0, -1):
+            sum = i * i
+            total = 1
+            if i == 5:
+                pass
+            for j in range(i, 0, -1):
+                while j > 0:
+                    while sum < target:
+                        sum += j * j
+                        total += 1
+                    if sum > target:
+                        if j > 0:
+                            sum -= j * j
+                            total -= 1
+                            j -= 1
+                        else:
+                            break
+                    if sum == target:
+                        if i in total_dict:
+                            if total < total_dict[i]:
+                                total_dict[i] = total
+                        else:
+                            total_dict[i] = total
+                        sum = i * i
+                        total = 1
+                        break
+
+
+        # now we need to find the min item in the dict
+        min_total = inf
+
+        for item in total_dict:
+            if total_dict[item] < min_total:
+                min_total = total_dict[item]
+
+        return min_total
+
+
+
+if __name__ == "__main__":
+    n = 43
+    Solution().numSquares(n)
