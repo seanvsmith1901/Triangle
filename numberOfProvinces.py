3ed520629bc58bcb77dc54d96e094878b5ef5715
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-11-14 14:02:11 -0700

    whatever
    thats finally working more than not - IDK why I am so bad about using queues lol

--- numberOfProvinces.py
+++ numberOfProvinces.py
@@ -1,52 +1,29 @@
 class Solution:
-
     def findCircleNum(self, isConnected) -> int:
-
-        # first we need to reverse all of the edges
-        # then run DFS on our reversed graph
-        # then find the SCCS from DSF (G and Post)
-        # then we can return that array.
-        # i migth still have the code
-
-        # so first I need to create a graph, where we are using a representation array
-        # and then I just need to return the total number of SCCs
-        # somehow I have to filter the SCCs to make sure they only have edges within teh SCC which
-        # is uhh going to be fun to say the least
-        currentList = []
+        # build the graph
         forwardGraph = {}
         for i in range(len(isConnected)):
             for j in range(len(isConnected)):
-                if i != j:
-                    if isConnected[i][j] == 1:
-                        if i in forwardGraph:
-                            forwardGraph[i].append(j)
-                        else:
-                            forwardGraph[i] = [j]
-        # now we have the graph in a representation that makes sense to me
-        # now we have to reverse it
-        reverseGraph = makeReverseGraph(forwardGraph)
-        forwardGraphNodes = makeNodes(forwardGraph)
-        backwardGraphnodes = makeNodes(reverseGraph)
-        _, postOrder = DFS(backwardGraphnodes, None)
+                if isConnected[i][j] == 1:
+                    if i not in forwardGraph:
+                        forwardGraph[i] = [j]
+                    else:
+                        forwardGraph[i].append(j)
 
-        SCCs, _ = DFS(forwardGraphNodes, postOrder)
+        reverseGraph = makeReverseGraph(forwardGraph) # make the reverse graph
 
-        offsetFactor = 0
-        totalNodesInScc = 0
-        for i in range(len(SCCs)):
-            for j in range(len(SCCs[i])):
-                totalNodesInScc += 1
-        if totalNodesInScc != len(isConnected) and totalNodesInScc < len(isConnected):
-            offsetFactor = len(isConnected) - totalNodesInScc
+        # first DFS function returns the postORder
+        postOrder = DFS(reverseGraph)
 
+        # the way we keep track of which ones we have visited is kinda wierd
+        visited = [False] * len(isConnected) # make a new array of visited (where every node corresponds to its spot in the dict
+        totalSccs = 0 # just so we know what they are
+        for node in postOrder: # make sure we are using hte post order version to sort these things
+            if not visited[node]:
+                totalSccs += 1 # we have found a new one, now figure out what he is connected to
+                DFS2(forwardGraph, node, visited)
 
-
-        # so all of this works solid, I just gotta figure out the rest lol.
-
-        # don't forget at the very end, if an node is not in the graph at all, then it is STRONGLY connected to itself lol
-        returnValue = len(SCCs) + offsetFactor
-        return returnValue
-
+        return totalSccs
 
 def makeReverseGraph(forwardGraph):
     reversedDict = {}
@@ -55,86 +32,40 @@ def makeReverseGraph(forwardGraph):
             reversedDict.setdefault(value, []).append(key)
     return reversedDict
 
+def DFS(graph):
+    # Standard iterative DFS for post-order traversal
+    visited = set()
+    postOrder = []
+    stack = []
 
+    # Initialize the stack with all nodes
+    for node in graph:
+        if node not in visited:
+            stack.append(node)
+            while stack:
+                node = stack[-1]
+                if node not in visited:
+                    visited.add(node)
+                    for neighbor in graph.get(node, []): # null handling
+                        if neighbor not in visited: # if we haven't seen him yet
+                            stack.append(neighbor) # throw him on top
+                else:
+                    stack.pop() # he has been visited we don't need him no more
+                    postOrder.append(node) # but we do want to throw him in the post order lsit
 
-def DFS(graph, tieBreakers):
-    if tieBreakers == None: # sets the tiebreakers if we don't have any
-        tieBreakers = []
-        for node in graph:
-            tieBreakers.append(node)
+    postOrder.reverse()  # reverse the post order to get it from greatest to least
+    return postOrder
 
+def DFS2(graph, node, visited):
+    stack = [node] # create a stack with just our node on it
+    while stack:
+        curr = stack.pop() # get the first one off (we want DFS, not BFS)
+        if not visited[curr]: # if he has not been visited
+            visited[curr] = True # mark him as visited
+            for neighbor in graph.get(curr, []): # get all of his freinds
+                if not visited[neighbor]: # if they are not in the visited dict yet
+                    stack.append(neighbor) # throw them on top
 
-
-
-    for node in tieBreakers: # resets the fetchers
-        node.unvisit()
-
-
-    totalSccs = []
-    postOrderStuff = []
-    for node in tieBreakers: # make sure to use the tiebreakers first
-        newList = []
-        newSCC = DFS2(node, newList, postOrderStuff, tieBreakers)
-        if newSCC is not None:
-            totalSccs.append(newSCC)
-
-    postOrderStuff.reverse()
-    return totalSccs, postOrderStuff
-
-def DFS2(node, newSCC, postOrderStuff, tieBreakers):
-    if not node.check():
-        node.visit()
-        for child in tieBreakers:
-            if node.ID == child.ID:
-                tieBreakers.remove(child)
-                break
-        if node.adjacentNodes != None:
-            for adjacentNode in node.adjacentNodes:
-                if not adjacentNode.check(): # he is an unchecked fetcher
-                    adjacentNode.visit()
-                    for child in tieBreakers:
-                        if adjacentNode.ID == child.ID:
-                            tieBreakers.remove(child)
-                            break
-
-                    if (adjacentNode.adjacentNodes) != None:
-                        for adjacent in adjacentNode.adjacentNodes:
-                            DFS2(adjacent, newSCC, postOrderStuff, tieBreakers)
-                    adjacentNode.visit()
-
-
-                    newSCC.append(adjacentNode)
-                    postOrderStuff.append(adjacentNode)
-        newSCC.append(node)
-        postOrderStuff.append(node)
-    return newSCC
-
-
-def makeNodes(graph):
-    newGraph = []
-    for i in range(len(graph)):
-        nodeList = []
-        for node in graph[i]:
-            nodeList.append(Node(node))
-        newNode = Node(i, nodeList)
-        newGraph.append(newNode)
-    return newGraph
-
-class Node:
-    def __init__(self, ID, newNodesList=None, previsit=None, postVisit=None):
-        self.ID = ID
-        self.visited = False
-        self.adjacentNodes = newNodesList
-        self.preVisit = previsit
-        self.postVisit = postVisit
-    def visit(self):
-        self.visited = True
-    def unvisit(self):
-        self.visited = False
-    def check(self):
-        return self.visited
-
-# find teh SCCS and return those lol
-
+# Test case
 if __name__ == '__main__':
-    Solution().findCircleNum([[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]])
+    Solution().findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]])
