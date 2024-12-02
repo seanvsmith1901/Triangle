a6217fb4b1ada62f8d88664008340e498bc6bb79
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-12-02 10:14:37 -0700

    whe
    whe

---
+++ minConnectedCost.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minCostConnectPoints(self, points) -> int:
+        pass
+        tree = [] # holds all of the points that we have so far
+        # points are passed in as a set of tuples with distances between them
+        # I think the easiest way to do this would be to use prims
+        # whee
